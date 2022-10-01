a83b56befb95a5f8b34dba3c8da5c89d7c433dc8
Mike Stumpf <mike.a.stumpf@gmail.com>
2022-10-01 15:42:02 +0000

    upgrade plugins

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '5.2.3' );
-define( 'REDIRECTION_BUILD', 'cb109e7c800603ff2d5cc098451320be' );
-define( 'REDIRECTION_MIN_WP', '4.6' );
+define( 'REDIRECTION_VERSION', '5.3.4' );
+define( 'REDIRECTION_BUILD', '04d536d277e6e20820f24f21d395db08' );
+define( 'REDIRECTION_MIN_WP', '5.4' );
