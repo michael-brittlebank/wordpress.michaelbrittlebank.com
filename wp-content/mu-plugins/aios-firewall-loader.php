43a13921170fd68b2685773057b4004fcc732e3b
Mike Stumpf <mike.a.stumpf@gmail.com>
2023-10-01 11:54:50 +0000

    Update WP to 6.3.1

---
+++ wp-content/mu-plugins/aios-firewall-loader.php
@@ -0,0 +1,6 @@
+<?php
+// Begin AIOWPSEC Firewall
+if (file_exists('/var/www/wordpress.michaelbrittlebank.com/aios-bootstrap.php')) {
+	include_once('/var/www/wordpress.michaelbrittlebank.com/aios-bootstrap.php');
+}
+// End AIOWPSEC Firewall
